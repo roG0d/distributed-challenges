69016da46f082e25ae61109e75d52922867ed2a5
roG0d <rodgarcas98@gmail.com>
2023-07-27 13:18:28 +0200

    Comments on the code

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,8 @@ pub struct Body<Payload> {
     pub payload: Payload,
 }
 
+
+// InitPayload struct
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag="type")]
 #[serde(rename_all="snake_case")]
@@ -29,6 +31,7 @@ enum InitPayload{
     InitOk,
 }
 
+// Init struct
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Init{
     pub node_id: String, 
@@ -37,10 +40,13 @@ pub struct Init{
 
 //Definition of trait Node
 pub trait Node<S, Payload> {
+
+    // Init for the specific node
     fn from_init(state: S, init: Init) -> anyhow::Result<Self> 
     where 
         Self:Sized;
 
+    // Protocol for the specific node
     fn step<'a> (&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()>;
 }
 
@@ -58,16 +64,19 @@ where
     // Lock the stdout for the thread
     let mut stdout = std::io::stdout().lock();
 
+    // Get the first msg from stdin to check if there's messages
     let init_msg: Message<InitPayload> = serde_json::from_str(
         &stdin.next()
         .expect("no init message received")
         .context("failed to read init message from stdin")?,
     ).context("init message could not be deserialized")?;
 
+    // Check if the first msg it's a init_msg
     let InitPayload::Init(init) = init_msg.body.payload else {
         panic!("first message should be init");
     };
     
+    // If so, initialize the node and send a initOk reply
     let mut node:N = Node::from_init(init_state, init).context("node initilization failed")?;
 
     let reply = Message {
@@ -83,6 +92,8 @@ where
     serde_json::to_writer(&mut stdout, &reply).context("serialize response to init")?;
     stdout.write_all(b"\n").context("write trailing newline")?;
 
+
+    // Continues with the rest of the protocols
     for line in stdin {
         let line = line.context("Maelstrom input from STDIN could not be read")?;
         let input: Message<P> = serde_json::from_str(&line)
